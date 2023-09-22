a07f2ab1efb48f542625df649e796b4ca9f85f4c
Philippe Miossec <pmiossec@gmail.com>
2023-09-22 16:35:07 +0200

    Save local changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import Main from './app/Main';
 import './index.scss';
+import { StrictMode } from 'react';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
