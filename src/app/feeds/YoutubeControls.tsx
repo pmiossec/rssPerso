4679967340f6eda9dc141f3ef7bc287feee0422d
Philippe Miossec <pmiossec@gmail.com>
2023-10-05 23:32:34 +0200

    feat: description for youtube feeds

--- src/app/feeds/YoutubeControls.tsx
+++ src/app/feeds/YoutubeControls.tsx
@@ -11,17 +11,14 @@ export default ({link, displayContent}: YoutubeContolsProps): JSX.Element => {
             ? url.replace('www.youtube.com', 'yewtu.be')
             : url;
 
-    const copyUrlToClipboard = () => navigator.clipboard.writeText(link.url);
     const shareUrl = () => navigator.share({url: link.url});
     
     return (<>
-        {/* <a onClick={copyUrlToClipboard} className="youtube">📋</a> */}
         {/* @ts-ignore */}
         {navigator.canShare && <a onClick={shareUrl} className="youtube">{link.title}</a>}
         {!navigator.canShare && <a href={convertToInvidiousUrl(link.url)}
-              onMouseOver={() => displayContent(link.content)} target="_blank" title="Watch on Invidious (privacy friendly!)">
+              onMouseOver={() => displayContent(link.content ? `<h1>${link.title}</h1>\n${link.content.replace('\n', '<br/>')}` : '')} target="_blank" >
             {link.title}
-            {/* <img src="invidious.png" alt="Watch on invidious" className="invidious" /> */}
         </a>
 }
     </>);
